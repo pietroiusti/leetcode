1198385b93763499c4593a4fda06558d34839cb2
glptrst <giulio.pietroiusti@gmail.com>
2022-07-11 11:46:56 +0200

    Simplify.

--- palindrome-number/palindrome-number-0.c
+++ palindrome-number/palindrome-number-0.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
+#include <limits.h>
 
 /* https://leetcode.com/problems/palindrome-number/ */
 /* */
@@ -11,51 +13,32 @@
 /* */
 /* An integer is a palindrome when it reads the same backward as forward. */
 
-// simple solution
+// Simple solution: reverse the number and compare it with the original.
 
-void itoa(uint32_t n, char s[]);
-void reverse(char s[], int l);
-int isPalindrome(int x);
+bool isPalindrome(int x);
 
 int main(void) {
-    if(isPalindrome(1023401) == 0) {
-        printf("is palidrome.\n");
-    } else {
-        printf("is not palindrome.\n");
-    }
+    int number = 101;
+    
+    if (isPalindrome(number))
+        printf("Is palindrome\n");
+    else
+        printf("Is not palindrome\n");
+
     return 0;
 }
 
-void itoa(uint32_t n, char s[])
-{
-    uint32_t i = 0;
-    do {
-        s[i] = n % 10 + '0';
-        n = n / 10;
-        i++;
-    } while (n != 0);
-    s[i] = '\0';
-    reverse(s, i);
-}
-
-// Reverse string s of length l
-void reverse(char s[], int l) {
-    int temp, i;
-    l--;
-    for (i = 0; i < l; i++, l--) {
-        temp = s[i];
-        s[i] = s[l];
-        s[l] = temp;
-    }
-}
-
-int isPalindrome(int x) {
-    char *s = malloc(2147483649*sizeof(char));
-    itoa(x, s);
-
-    char *reversed = malloc(2147483649*sizeof(char));
-    itoa(x, reversed);
-    reverse(reversed, strlen(reversed));
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
     
-    return strcmp(s, reversed);    
+    long reversed = 0;
+    int x_copy = x;
+    
+    while (x_copy != 0) {
+        reversed = reversed*10 + x_copy%10;
+        x_copy = x_copy / 10;
+    }
+
+    return x == reversed;
 }
